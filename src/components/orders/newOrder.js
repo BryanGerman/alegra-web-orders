9c2fcd8a7679cf4c4c37c2f69786004464b9e7f4
bryanlema <bryanglema2@gmail.com>
2024-05-01 14:13:59 -0500

    Agregar funcionalidad a botón de orden

--- src/components/orders/newOrder.js
+++ src/components/orders/newOrder.js
@@ -1,8 +1,20 @@
-const handleClick = () => {
-    console.log(" click! ")
-}
+import { useState, useCallback } from "react"
+import axios from 'axios';
 
 export const NewOrder = () => {
+
+    const [isSending, setIsSending] = useState(false);
+    const [order, setOrder] = useState({});
+
+    const handleClick = useCallback(async () => {
+        if (isSending) return
+
+        setIsSending(true)
+        const order_received = await axios.get('https://liberal-kora-bryancompany.koyeb.app/api/v1/orders/new')
+        setOrder(order_received)
+        setIsSending(false)
+
+    }, [isSending]);
     return (
         <>
             <button className="button1" onClick={handleClick}> <b>Generar Orden</b> </button>
